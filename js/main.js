14c93f812a4719ed8af7b7f2264a0f38b5d8d83f
temalyalin <artemij0208@gmail.com>
2020-11-26 22:12:10 +0300

    create: added revievs section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
 
   keyboard: {
@@ -13,3 +13,25 @@ var mySwiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 })
+ymaps.ready(init);
+        function init(){
+            var myMap = new ymaps.Map("map", {
+                center: [55.76, 37.64],
+                zoom: 7
+            });
+        }
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+})        
